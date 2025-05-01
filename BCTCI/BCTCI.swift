f21eea0e2c9d43b949366a41d2e061e5d56621d1
Tri Pham <phamminhtris@outlook.com>
2025-04-30 20:17:56 -0500

    Initial Commit

---
+++ BCTCI/BCTCI.swift
@@ -0,0 +1,9 @@
+//
+//  BCTCI.swift
+//  BCTCI
+//
+//  Created by Tri Pham on 4/30/25.
+//
+
+import Foundation
+
---
+++ BCTCITests/BCTCITests.swift
@@ -0,0 +1,17 @@
+//
+//  BCTCITests.swift
+//  BCTCITests
+//
+//  Created by Tri Pham on 4/30/25.
+//
+
+import Testing
+@testable import BCTCI
+
+struct BCTCITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
