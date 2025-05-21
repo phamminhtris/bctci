47450a9716c066ffe3997c66170e165585a6561b
Tri Pham <phamminhtris@outlook.com>
2025-05-21 07:16:59 -0500

    Save

--- BCTCI/TwoPointers.swift
+++ BCTCI/TwoPointers.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Problem 27.8
 // Input: [2, 3, 3, 4, 5, 7], [3, 3, 9], [3, 3, 9]
 func threeWayMergeWithoutDuplicates(arr1: [Int], arr2: [Int], arr3: [Int]) -> [Int] {
     var i = 0, j = 0, k = 0
@@ -41,5 +42,58 @@ func threeWayMergeWithoutDuplicates(arr1: [Int], arr2: [Int], arr3: [Int]) -> [I
         }
 
     }
+    return res
+}
+
+// Problem 27.9
+// Input: [8, 4, 2, 6]
+// Output: [2, 4, 6, 8]
+func sortValleyShapeArray(_ arr: [Int]) -> [Int] {
+    var l = 0, r = arr.count - 1
+    var res = Array(repeating: 0, count: arr.count)
+    var i = arr.count - 1
+    while l < r {
+        if arr[l] < arr[r] {
+            res[i] = arr[r]
+            i -= 1
+            r -= 1
+        } else {
+            res[i] = arr[l]
+            l += 1
+            i -= 1
+        }
+    }
+    res[0] = arr[l]
+    return res
+}
+
+func missingNumbersInRange(_ arr: [Int], low: Int, high: Int) -> [Int] {
+    var i = 0
+    var current = low
+    var res = [Int]()
+    
+    while current <= high {
+        if i >= arr.count {
+            break
+        }
+        let elem = arr[i]
+        if elem < current {
+            // either already added or out of range
+            i += 1
+        } else if elem == current {
+            // found common element
+            i += 1
+            current += 1
+        } else {
+            res.append(current)
+            current += 1
+        }
+    }
+
+    while current <= high {
+        res.append(current)
+        current += 1
+    }
+
     return res
 }
--- BCTCITests/TwoPointersTests.swift
+++ BCTCITests/TwoPointersTests.swift
@@ -25,4 +25,34 @@ struct TwoPointersTests {
             ) == result
         )
     }
+
+    @Test("Sort Valley-Shape Array", arguments: [
+        ([8, 4, 2, 6], [2, 4, 6, 8]),
+        ([1, 2], [1, 2]),
+        ([1, 1, 2, 2], [1, 1, 2, 2])
+    ])
+    func sortValleyShapeArray(inputs: ([Int], [Int])) {
+        let (arr, result) = inputs
+        #expect(BCTCI.sortValleyShapeArray(arr) == result)
+    }
+
+    struct MissinNumbersInRangeInput {
+        let arr: [Int]
+        let range: ClosedRange<Int>
+        let result: [Int]
+    }
+
+    @Test("Missing Numbers in Range", arguments: [
+        MissinNumbersInRangeInput(arr: [6, 9, 12, 15, 18], range: 9...13, result: [10, 11, 13]),
+        MissinNumbersInRangeInput(arr: [], range: 9...9, result: [9]),
+        MissinNumbersInRangeInput(arr: [6, 7, 8, 9], range: 7...8, result: [])
+    ])
+    func missingNumbersInRange(_ input: MissinNumbersInRangeInput) {
+        let res = BCTCI.missingNumbersInRange(
+                input.arr,
+                low: input.range.lowerBound,
+                high: input.range.upperBound
+        )
+        #expect(res == input.result)
+    }
 }
